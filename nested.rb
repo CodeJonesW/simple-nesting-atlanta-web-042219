4a76c33c7401efd3d961a7fd6e949452e83e82fa
Will Jones <willjones2010@gmail.com>
2019-04-18 13:18:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,21 @@
 
-def hopper(hash)
-puts hash[:grace_hopper]
+def hopper
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+return programmer_hash[:grace_hopper]
 
 end
 
